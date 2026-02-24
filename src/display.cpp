87b5fd3f706ac779ef03ba215aafe1f91afb89c4
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-02-24 10:57:59 -0300

    [docs] pinout spec

--- src/display.cpp
+++ src/display.cpp
@@ -2,7 +2,7 @@
 #include <U8g2lib.h>
 #include <Wire.h>
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 22, 21);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 22, 21); // I2C Display PIN 22 SCL, PIN 21 SDA
 
 bool hasDisplay = false;
 
